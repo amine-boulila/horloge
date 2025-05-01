ff834dc438767f46e9b844449b2acfe13cdeb665
amine boulila <amin.boulila@gmail.com>
2025-05-01 23:13:49 +0100

    made the execution dynamic

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("Usage: java Main <1=scalar | 2=vector | 3=matrix>");
+            return;
+        }
+
+        String clockType;
+        switch (args[0]) {
+            case "1":
+                clockType = "scalar";
+                break;
+            case "2":
+                clockType = "vector";
+                break;
+            case "3":
+                clockType = "matrix";
+                break;
+            default:
+                System.out.println("Invalid argument. Use 1 for scalar, 2 for vector, 3 for matrix.");
+                return;
+        }
+
         int[] ports = {5000, 5001, 5002, 5003};
-        String clockType = "matrix"; // Change to "vector" or "matrix" as needed
         Interface visualizer = new Interface();
         Process.setVisualizer(visualizer);
-
 
         for (int i = 0; i < 4; i++) {
             int[] others = new int[3];
